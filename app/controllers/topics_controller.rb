961cc90afb269693b48995064a11cb2d1ae144cc
purpleice9765 <purpleice9765@msn.com>
2015-10-01 15:00:55 +0800

    new 放到首頁

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,9 +1,17 @@
 class TopicsController < ApplicationController
 
-	before_action :topic_params_id, :only =>[ :show, :edit, :update, :destroy]
+	before_action :topic_params_id, :only =>[ :show, :update, :destroy]
 
 	def index
 		@topics = Topic.all
+
+		#如果出現 tipic_id 代表針對個案id處理edit
+		if params[:topic]
+  			@topic = Topic.find( params[:topic] )
+  		else
+			@topic = Topic.new
+  		end
+
 	end
 
 	def new
@@ -11,24 +19,34 @@ class TopicsController < ApplicationController
 	end
 
 	def create
-		@topic = Topic.new( topic_params)
-		@topic.save
 
-		redirect_to topics_path
+		@topic = Topic.new( topic_params)
+		if @topic.save
+			redirect_to topics_path
+			flash[:notice] = "Create Success"
+		elsif 
+			@topics = Topic.all
+			flash[:alert] = "Create Fail"
+			render "index"
+		end
+
 	end	
 
 	def show
 
 	end
 
-	def edit
 
-	end
 
 	def update
-		@topic.update(topic_params)
-		flash[:notice] = "Update Success!"
-		redirect_to topics_path
+		if @topic.update(topic_params)
+			flash[:notice] = "Update Success!"
+			redirect_to topics_path
+		else 
+			@topics = Topic.all
+			flash[:alert] = "Update Fail"
+			render "index"
+		end
 	end
 
 	def destroy
