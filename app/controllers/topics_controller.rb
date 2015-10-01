af0a71527243c02f9f73a48a0fd5eeaea3f64cfd
purpleice9765 <purpleice9765@msn.com>
2015-10-01 13:02:24 +0800

    new, show

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,48 @@
+class TopicsController < ApplicationController
+
+	before_action :topic_params_id, :only =>[ :show, :edit, :update, :destroy]
+
+	def index
+		@topics = Topic.all
+	end
+
+	def new
+		@topic = Topic.new
+	end
+
+	def create
+		@topic = Topic.new( topic_params)
+		@topic.save
+
+		redirect_to topics_path
+	end	
+
+	def show
+
+	end
+
+	def edit
+		
+	end
+
+	def update
+		
+	end
+
+	def destroy
+		
+	end
+
+
+	private
+
+	def topic_params
+		params.require(:topic).permit(:name, :content)
+	end
+
+	def topic_params_id
+		@topic =Topic.find(params[:id])
+	end
+
+
+end
