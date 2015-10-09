d13fae0896aa2d27caeb0f58c46bfcd4b6f25496
purpleice9765 <purpleice9765@msn.com>
2015-10-09 17:21:26 +0800

    username 獨一性

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   validates_presence_of :email, :username
+  validates_uniqueness_of :username
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
